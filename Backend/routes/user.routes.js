27ea5dca771280ccb19b8c7037bf4c8f3bd861ee
Anuj Raj <akgupta.anuj@gmail.com>
2025-02-15 13:14:22 +0530

    Refactor user routes for improved readability by removing unnecessary comments and organizing middleware

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,43 +1,34 @@
 const express = require('express');
 const router = express.Router();
-// Controller import
 const userController = require('../controllers/user.controller');
-
-console.log("userController :", userController);
 // Authentication middleware
 const { authMiddleware } = require('../middlewares/auth.middleware'); 
-
 // Validation middleware import
 const {
   validateRequest,
   registerValidationRules,
   loginValidationRules,
 } = require('../middlewares/validationMiddleware');
-
 // Registering a new user
 router.post(
   '/register',
-  registerValidationRules,   // Validation rules
-  validateRequest,            // Validation middleware
+  registerValidationRules,   
+  validateRequest,
   userController.registerUser
 );
-
 // Login a user
 router.post(
   '/login',
-  loginValidationRules,      // Validation rules
-  validateRequest,           // Validation middleware
+  loginValidationRules,
+  validateRequest,
   userController.loginUser
 );
-
 // Logout a user
 router.get(
   '/logout', 
   authMiddleware, 
   userController.logoutUser
 );
-
-
 // Get user profile
 router.get(
   '/profile', 
